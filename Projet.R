3286aabb821cf0bb4130120ecdf21506be13de1e
Nambrok <spaceship84@gmail.com>
2018-11-10 17:43:48 +0100

    Added somes changes on the linear model, added others models (Romain) Added RandomForest model and a plot to see how much tree can be used for the randomForest

--- Projet.R
+++ Projet.R
@@ -4,6 +4,14 @@ rm(list=ls())
 # Projet Robert Romain Thenot Damien 
 # Student Performance Data Set : https://archive.ics.uci.edu/ml/datasets/Student+Performance
 #---------------------
+# install.packages("corrplot")
+# install.packages("hydroGOF")
+# install.packages("randomForest")
+
+## LIBRARIES
+library(corrplot)
+library(hydroGOF)
+library(randomForest)
 
 d1=read.table("student-mat.csv",sep=";",header=TRUE,as.is = FALSE)
 d2=read.table("student-por.csv",sep=";",header=TRUE)
@@ -70,7 +78,7 @@ d2[,23] = as.numeric(d2[,23])
 
 mcord1 = cor(d1)
 mcord2 = cor(d2)
-library(corrplot)
+
 corrplot(mcord2, type="upper", col=c("black", "white"),
          bg="lightblue")
 #corrplot(mcord1, type="upper", col=c("black", "white"),
@@ -81,7 +89,8 @@ yd2.G2 = d2[,32]
 yd2.G3 = d2[,33] 
 
 xd2.G1 = cbind(d2[,0:30],d2[,32:33])
-xd2.G2 = cbind(d2[,0:31],yd2.G3)
+xd2.G2 = cbind(d2[,0:31],d2[,33])
+names(xd2.G2)[32]<-"G3"
 xd2.G3 = d2[,0:32]
 #Matrice decouper pour les notes de chaques pÃ©riode en 90 % train et 10 % test
 yd2.G1.train = yd2.G1[1:584]
@@ -92,17 +101,17 @@ yd2.G3.train = yd2.G3[1:584]
 yd2.G3.test = yd2.G3[585:649]
 
 d2.G1.train = d2[1:584,]
-d2.G1.test = d2[585:659,]
+d2.G1.test = d2[585:649,]
 xd2.G1.train = xd2.G1[1:584,]
-xd2.G1.test = xd2.G1[585:659,]
+xd2.G1.test = xd2.G1[585:649,]
 d2.G2.train = d2[1:584,]
-d2.G2.test = d2[585:659,]
+d2.G2.test = d2[585:649,]
 xd2.G2.train = xd2.G2[1:584,]
-xd2.G2.test = xd2.G2[585:659,]
+xd2.G2.test = xd2.G2[585:649,]
 d2.G3.train = d2[1:584,]
-d2.G3.test = d2[585:659,]
+d2.G3.test = d2[585:649,]
 xd2.G3.train = xd2.G3[1:584,]
-xd2.G3.test = xd2.G3[585:659,]
+xd2.G3.test = xd2.G3[585:649,]
 
 G1.lm = step(lm(G1~ .,d2.G1.train), direction='backward')
 
@@ -112,9 +121,51 @@ G2.lm = step(lm(G2~ .,d2.G2.train), direction='backward')
 
 G3.lm = step(lm(G3~ .,d2.G3.train), direction='backward')
 
-summary(G1.lm)
-summary(G2.lm)
-summary(G3.lm)
+# summary(G1.lm)
+# summary(G2.lm)
+# summary(G3.lm)
 
-predict(G1.lm,xd2.G1.test,interval='prediction',level=0.95)
+#Modele AIC
+G1.lm = step(lm(G1~ .,d2.G1.train), direction='backward')
+G2.lm = step(lm(G2~ .,d2.G2.train), direction='backward')
+G3.lm = step(lm(G3~ .,d2.G3.train), direction='backward')
+#Modele en utilisant tout les attributs
+G1.all.lm = lm(G1~ .,d2.G1.train)
+G2.all.lm = lm(G2~ .,d2.G2.train)
+G3.all.lm = lm(G3~ .,d2.G3.train)
+#Modele basé sur la matrice de correlation
+G1.cr.lm = update(G1.all.lm,.~.-school-sex-age-guardian-traveltime-failures-schoolsup-paid-romantic-freetime-goout-Dalc-Walc-health-absences)
+G2.cr.lm = update(G2.all.lm,.~.-school-sex-age-guardian-traveltime-failures-schoolsup-paid-romantic-freetime-goout-Dalc-Walc-health-absences)
+G3.cr.lm = update(G3.all.lm,.~.-school-sex-age-guardian-traveltime-failures-schoolsup-paid-romantic-freetime-goout-Dalc-Walc-health-absences)
+#modèle randomforest
+G1.rf.lm = randomForest(G1~., data=d2.G1.train, ntree=500, na.action = na.omit)
+G2.rf.lm = randomForest(G2~., data=d2.G2.train, ntree=500, na.action = na.omit)
+G3.rf.lm = randomForest(G3~., data=d2.G3.train, ntree=500, na.action = na.omit)
 
+# predictionG1 = predict(G1.lm,xd2.G1.test,interval='prediction',level=0.95)
+# predictionG2 = predict(G2.lm,xd2.G2.test,interval='prediction',level=0.95)
+# predictionG3 = predict(G3.lm,xd2.G3.test,interval='prediction',level=0.95)
+
+predictionAllAttributes = predict(G1.all.lm, xd2.G1.test, interval='prediction', level=0.95)
+rmse(predictionAllAttributes[, "fit"], yd2.G1.test)
+
+predictionCr = predict(G1.cr.lm, xd2.G1.test, interval='prediction', level=0.95)
+rmse(predictionCr[, "fit"], yd2.G1.test)
+
+predictionAIC = predict(G1.lm,xd2.G1.test,interval='prediction',level=0.95)
+rmse(predictionAIC[, "fit"], yd2.G1.test)
+
+predictionRF = predict(G1.rf.lm, xd2.G1.test, interval='prediction', level=0.95)
+rmse(predictionRF, yd2.G1.test)
+
+resultRMSERandomForest = {}
+for(ntree in c(500, 1000, 1500, 2000)){
+  rf.mod = randomForest(G1~., data=d2.G1.train, ntree=ntree, na.action = na.omit)
+  predictionRFNTree = predict(rf.mod, xd2.G1.test, interval='prediction', level=0.95)
+  resultRMSERandomForest[ntree] = rmse(predictionRFNTree, yd2.G1.test)
+}
+plot(resultRMSERandomForest)
+
+# rmse(predictionG1[, "fit"], yd2.G1.test)
+# rmse(predictionG2[, "fit"], yd2.G2.test)
+# rmse(predictionG3[, "fit"], yd2.G3.test)
